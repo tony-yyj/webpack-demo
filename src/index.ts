0e8bd1e49e84359bc73a914b4ebcc6ec9429e1fb
tony <tonycs_y@126.com>
2020-05-12 11:40:09 +0800

    ts-loader

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function getName(name: string) {
+    console.log('name', name);
+}
+
+getName('tony');
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.ts'
+    },
     mode: "development",
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
     },
     plugins: [
         new CleanWebpackPlugin(),
-
         new HtmlWebpackPlugin({
             title: "webpack demo",
             filename: "index.html",
