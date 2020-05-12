2e2260df8466a9bbc21aa98c6f60bdeffbb2307a
tony <tonycs_y@126.com>
2020-05-12 11:11:38 +0800

    clear webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
+    mode: "development",
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
 };
