cc204f4504eed110f01780ecce1b07cd00bd50f8
tony <tonycs_y@126.com>
2020-05-12 11:27:02 +0800

    copy webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -11,11 +12,18 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
+
         new HtmlWebpackPlugin({
             title: "webpack demo",
             filename: "index.html",
             template: "src/index.html",
             inject: "body",
         }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/assets',
+                to: 'assets'
+            },
+        ])
     ],
 };
